8ca4968ec1f9110be48466dfe848019dc19fd947
EcodeJR <emmanueldcode@gmail.com>
2026-04-02 13:39:38 +0100

    feat: add environment configuration and update API proxy settings

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,22 @@
-import { defineConfig } from 'vite'
+/* eslint-disable no-undef */
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api'),
+export default defineConfig(({ _command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL || 'http://localhost:3001',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '/api'),
+        },
       },
     },
-  },
+  }
 })
